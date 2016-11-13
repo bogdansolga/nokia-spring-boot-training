1a10b4244281c1f2c6bd391dca7baeea104ed896
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-13 17:35:32 +0200

    [dev] Added the bean qualifying sample

---
+++ d01/d01s04/d01s04e02-bean-qualifying/src/main/java/com/oce/springboot/training/d01/s04/config/BeanQualifyingConfig.java
@@ -0,0 +1,26 @@
+package com.oce.springboot.training.d01.s04.config;
+
+import com.oce.springboot.training.d01.s04.repository.ProductRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A simple Spring configuration, which exposes two {@link ProductRepository} {@link Bean}s
+ *
+ * @author bogdan.solga
+ */
+@Configuration
+@ComponentScan(basePackages = "com.oce.springboot.training")
+public class BeanQualifyingConfig {
+
+    @Bean
+    public ProductRepository mySQLProductRepository() {
+        return new ProductRepository("MySQL");
+    }
+
+    @Bean
+    public ProductRepository oracleProductRepository() {
+        return new ProductRepository("Oracle");
+    }
+}
