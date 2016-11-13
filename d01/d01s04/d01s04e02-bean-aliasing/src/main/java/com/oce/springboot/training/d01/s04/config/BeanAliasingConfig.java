295e0ee17aaccdbeb0bff3c2c8f64e453e67bf30
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-13 16:49:06 +0200

    [dev] Added a small bean aliasing sample

---
+++ d01/d01s04/d01s04e02-bean-aliasing/src/main/java/com/oce/springboot/training/d01/s04/config/BeanAliasingConfig.java
@@ -0,0 +1,19 @@
+package com.oce.springboot.training.d01.s04.config;
+
+import com.oce.springboot.training.d01.s04.repository.ProductRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A simple Spring configuration, which exposes a single aliased bean
+ *
+ * @author bogdan.solga
+ */
+@Configuration
+public class BeanAliasingConfig {
+
+    @Bean(name = {"productRepository", "repository", "prodRepo"})
+    public ProductRepository productRepository() {
+        return new ProductRepository();
+    }
+}
