c668ada5e82a2f9e9a332872227d0e17e98b7c12
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-18 07:06:35 +0200

    [dev] Added a properties and an yaml usage sample

---
+++ d02/d02s02/d02s02e01-properties-files/src/main/java/com/oce/springboot/training/d02/s02/PropertiesDemoApp.java
@@ -0,0 +1,12 @@
+package com.oce.springboot.training.d02.s02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertiesDemoApp.class, args);
+    }
+}
