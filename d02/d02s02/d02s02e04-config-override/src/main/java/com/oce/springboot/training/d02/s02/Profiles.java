3daa5634ff0d4c48340508479ddc3f9215a96218
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-19 20:56:37 +0200

    [dev] Added a properties overriding sample

---
+++ d02/d02s02/d02s02e04-config-override/src/main/java/com/oce/springboot/training/d02/s02/Profiles.java
@@ -0,0 +1,13 @@
+package com.oce.springboot.training.d02.s02;
+
+/**
+ * The used run profiles
+ *
+ * @author bogdan.solga
+ */
+public final class Profiles {
+
+    public static final String DEV = "dev";
+
+    public static final String PROD = "prod";
+}
