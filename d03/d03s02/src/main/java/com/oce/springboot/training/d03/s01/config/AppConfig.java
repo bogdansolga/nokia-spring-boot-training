ae5bf27b1ae5c828f013de39efe1f94ddc8aed05
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-21 21:46:36 +0200

    [dev] Added a draft security config

---
+++ d03/d03s02/src/main/java/com/oce/springboot/training/d03/s01/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.oce.springboot.training.d03.s01.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.oce.springboot.training.d03.s02")
+public class AppConfig {
+}
