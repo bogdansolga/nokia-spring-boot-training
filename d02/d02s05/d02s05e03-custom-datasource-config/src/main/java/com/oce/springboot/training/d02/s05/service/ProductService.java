58c21702133dbfbf62cf2d8291f1550ef671195e
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-22 18:58:14 +0200

    [improve] Added the transactional annotations

--- d02/d02s05/d02s05e03-custom-datasource-config/src/main/java/com/oce/springboot/training/d02/s05/service/ProductService.java
+++ d02/d02s05/d02s05e03-custom-datasource-config/src/main/java/com/oce/springboot/training/d02/s05/service/ProductService.java
@@ -4,6 +4,8 @@ import com.oce.springboot.training.d02.s05.model.Product;
 import com.oce.springboot.training.d02.s05.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 
@@ -24,18 +26,22 @@ public class ProductService {
         create(product);
     }
 
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void create(final Product product) {
         productRepository.save(product);
     }
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Product get(final int id) {
         return productRepository.findOne(id);
     }
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Iterable<Product> getAll() {
         return productRepository.findAll();
     }
 
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void update(final int id, final Product product) {
         final Product existingProduct = productRepository.findOne(id);
 
@@ -44,6 +50,7 @@ public class ProductService {
         productRepository.save(product);
     }
 
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void delete(final int id) {
         productRepository.delete(id);
     }
