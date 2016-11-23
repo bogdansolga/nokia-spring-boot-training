e0f2103a5264958db790ceb0cdbc3ee004ee47ae
Bogdan Solga <bogdan.solga@gmail.com>
2016-11-23 14:49:36 +0200

    [dev] Added a security usage sample

--- d03/d03s02/src/main/java/com/oce/springboot/training/d03/s01/config/SecurityConfiguration.java
+++ d03/d03s02/src/main/java/com/oce/springboot/training/d03/s01/config/SecurityConfiguration.java
@@ -1,19 +1,33 @@
 package com.oce.springboot.training.d03.s01.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder())
             .withUser("user")
             .password("password")
             .roles("USER");
@@ -22,8 +36,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/resources/static/**", "/about").permitAll()
+                .antMatchers(HttpMethod.POST, "/admin").hasAnyRole("ADMIN", "MANAGER")
                 .anyRequest().authenticated()
                 .and()
         .formLogin();
+
+        /*
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        authentication.setAuthenticated(true);
+        */
+
+        // create 'manual' authentication
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken("john", "doe",
+                Collections.singleton(new SimpleGrantedAuthority("ADMIN")));
+        authentication.setAuthenticated(true);
+        securityContext.setAuthentication(authentication);
+
+        // obtaining the security context
+        SecurityContext existingContex = SecurityContextHolder.getContext();
+        existingContex.getAuthentication();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
