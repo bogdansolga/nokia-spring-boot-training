914c67bb2e9389331d47e2588ca34f9a0aa03c35
Bogdan Solga <bogdan.solga@gmail.com>
2016-12-08 10:18:32 +0200

    [fix] Commented the JDBC session usage

--- d03/d03s02/src/main/java/com/oce/springboot/training/d03/s01/config/AppConfig.java
+++ d03/d03s02/src/main/java/com/oce/springboot/training/d03/s01/config/AppConfig.java
@@ -2,10 +2,9 @@ package com.oce.springboot.training.d03.s01.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @Configuration
 @ComponentScan(basePackages = "com.oce.springboot.training.d03.s01")
-@EnableJdbcHttpSession
+//@EnableJdbcHttpSession
 public class AppConfig {
 }
