4c34077f1ac9aa808257c0aba0e5929223fb7e0c
Bogdan Solga <bogdan.solga@gmail.com>
2016-12-09 08:19:13 +0200

    [dev] Added two conditional beans samples

--- d03/d03s01/d03s01e01-conditional-annotations/src/main/java/com/oce/springboot/training/d03/s01/ConditionalBeansConfig.java
+++ d03/d03s01/d03s01e01-conditional-annotations/src/main/java/com/oce/springboot/training/d03/s01/ConditionalBeansConfig.java
@@ -2,6 +2,8 @@ package com.oce.springboot.training.d03.s01;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +27,18 @@ public class ConditionalBeansConfig {
     @ConditionalOnProperty(name = "matching.property", havingValue = "true")
     public String conditionalOnProperty() {
         return "Conditional on a property";
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "used.webserver", havingValue = "jetty")
+    public EmbeddedServletContainer jetty() {
+        return new JettyEmbeddedServletContainer(null, true);
+    }
+
+    // a 'feature toggle' sample approach
+    @Bean
+    @ConditionalOnProperty(name = "cool.feature", havingValue = "enabled")
+    public Object coolFeature() {
+        return new Object();
     }
 }
