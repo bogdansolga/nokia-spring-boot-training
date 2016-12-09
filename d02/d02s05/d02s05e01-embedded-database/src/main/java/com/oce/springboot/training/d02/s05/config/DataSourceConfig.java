1272da23282c39f025f912bca04112df369d384a
Bogdan Solga <bogdan.solga@gmail.com>
2016-12-09 08:26:42 +0200

    [dev] Added a few samples on how to wire a JDBC and a Hibernate template

--- d02/d02s05/d02s05e01-embedded-database/src/main/java/com/oce/springboot/training/d02/s05/config/DataSourceConfig.java
+++ d02/d02s05/d02s05e01-embedded-database/src/main/java/com/oce/springboot/training/d02/s05/config/DataSourceConfig.java
@@ -3,10 +3,23 @@ package com.oce.springboot.training.d02.s05.config;
 import com.oce.springboot.training.d02.s05.model.Product;
 import com.oce.springboot.training.d02.s05.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.Collections;
 
 /**
  * A simple {@link javax.sql.DataSource} configuration, which:
@@ -16,14 +29,53 @@ import javax.annotation.PostConstruct;
  * </ul>
  */
 @Configuration
-@EnableJpaRepositories(basePackages = "com.oce.springboot.training.d02.s05.repository")
+@EnableJpaRepositories(
+        basePackages = "com.oce.springboot.training.d02.s05.repository",
+        entityManagerFactoryRef = "entityManagerFactory"
+)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
+    private final DataSource dataSource;
     private final ProductService productService;
 
     @Autowired
-    public DataSourceConfig(final ProductService productService) {
-        this.productService = productService;
+    public DataSourceConfig(final DataSource dataSource, final ProductService productService) {
+        this.dataSource = dataSource; this.productService = productService;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public HibernateTemplate hibernateTemplate() {
+        final HibernateTemplate hibernateTemplate = new HibernateTemplate(null);
+
+        return hibernateTemplate;
+    }
+
+    @Profile("PROD")
+    @Bean
+    public CacheManager cacheManager() {
+        final SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+
+        Cache cache = new ConcurrentMapCache("products");
+        simpleCacheManager.setCaches(Collections.singleton(cache));
+
+        return simpleCacheManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf =  new LocalContainerEntityManagerFactoryBean();
+
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setPersistenceUnitName("default");
+        emf.setPackagesToScan("com.oce.springboot.training.d02.s05.repository");
+
+        return emf;
     }
 
     @PostConstruct
