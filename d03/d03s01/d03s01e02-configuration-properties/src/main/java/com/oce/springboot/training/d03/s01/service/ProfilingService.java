83e222b9697db73d5d05ede8eb47c7433805bf6c
Bogdan Solga <bogdan.solga@gmail.com>
2016-12-09 08:42:39 +0200

    [dev] Wired the profiling config

--- d03/d03s01/d03s01e02-configuration-properties/src/main/java/com/oce/springboot/training/d03/s01/service/ProfilingService.java
+++ d03/d03s01/d03s01e02-configuration-properties/src/main/java/com/oce/springboot/training/d03/s01/service/ProfilingService.java
@@ -1,5 +1,7 @@
 package com.oce.springboot.training.d03.s01.service;
 
+import com.oce.springboot.training.d03.s01.config.ProfilingConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,8 @@ import javax.annotation.PostConstruct;
 
 @Service
 public class ProfilingService {
+
+    private final ProfilingConfiguration profilingConfiguration;
 
     @Value("${profiling.environment}")
     private String profilingEnvironment;
@@ -17,10 +21,17 @@ public class ProfilingService {
     @Value("${profiling.connection.socket-timeout}")
     private String connectionSocketTimeout;
 
+    @Autowired
+    public ProfilingService(final ProfilingConfiguration profilingConfiguration) {
+        this.profilingConfiguration = profilingConfiguration;
+    }
+
     @PostConstruct
     public void displayProperties() {
         System.out.println("Profiling environment - " + profilingEnvironment);
         System.out.println("Profiling connection timeout - " + profilingConnectionTimeout);
         System.out.println("Connection socket timeout - " + connectionSocketTimeout);
+
+        System.out.println(profilingConfiguration.getEnvironment());
     }
 }
