e8ef570647b4fc646640bf2e5f7cb9e4ab338fa1
Bogdan Solga <bogdan.solga@gmail.com>
2017-01-19 19:21:46 +0200

    [improve] Wrapped the productRepository in an Optional

--- d01/d01s02/src/main/java/com/oce/springboot/training/d01/s02/service/ProductService.java
+++ d01/d01s02/src/main/java/com/oce/springboot/training/d01/s02/service/ProductService.java
@@ -2,6 +2,8 @@ package com.oce.springboot.training.d01.s02.service;
 
 import com.oce.springboot.training.d01.s02.repository.ProductRepository;
 
+import java.util.Optional;
+
 /**
  * A simple product service, which uses a {@link ProductRepository} as a collaborator
  *
@@ -12,7 +14,7 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public void displayProducts() {
-        productRepository.displayProducts();
+        Optional.ofNullable(productRepository).ifPresent(ProductRepository::displayProducts);
     }
 
     public void setProductRepository(ProductRepository productRepository) {
