e358088d92360fb82922f3bdb0c571125df4c5d2
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-04 07:35:34 +0300

    [fix + clean] Added the @EnableAsync annotation, cleaned the scheduled config

--- d04/d04s01/d04s01e03-task-executors/src/main/java/com/nokia/springboot/training/d04/s01/config/TaskExecutorsConfig.java
+++ d04/d04s01/d04s01e03-task-executors/src/main/java/com/nokia/springboot/training/d04/s01/config/TaskExecutorsConfig.java
@@ -3,35 +3,16 @@ package com.nokia.springboot.training.d04.s01.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
-@EnableScheduling
+@EnableAsync
 public class TaskExecutorsConfig {
 
     private final int PROCESSORS_COUNT = Runtime.getRuntime().availableProcessors();
-
-    @Bean
-    public TaskScheduler taskScheduler() {
-        final ThreadPoolTaskScheduler threadPoolTaskScheduler =  new ThreadPoolTaskScheduler();
-
-        threadPoolTaskScheduler.setPoolSize(PROCESSORS_COUNT);
-        threadPoolTaskScheduler.setThreadGroupName("scheduler-thread-pool");
-        threadPoolTaskScheduler.setThreadNamePrefix("scheduler-thread-");
-        threadPoolTaskScheduler.setThreadPriority(Thread.MIN_PRIORITY);
-        threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true);
-        threadPoolTaskScheduler.setAwaitTerminationSeconds(20);
-        threadPoolTaskScheduler.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        threadPoolTaskScheduler.initialize();
-
-        return threadPoolTaskScheduler;
-    }
 
     @Primary
     @Bean
@@ -55,20 +36,20 @@ public class TaskExecutorsConfig {
 
     @Bean
     public ThreadPoolTaskExecutor shortLivedTasksExecutor() {
-        final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        final ThreadPoolTaskExecutor shortLivedTaskExecutor = new ThreadPoolTaskExecutor();
 
-        threadPoolTaskExecutor.setCorePoolSize(PROCESSORS_COUNT);
-        threadPoolTaskExecutor.setMaxPoolSize(PROCESSORS_COUNT * 2);
-        threadPoolTaskExecutor.setKeepAliveSeconds(10);
-        threadPoolTaskExecutor.setQueueCapacity(100);
-        threadPoolTaskExecutor.setThreadGroupName("short-lived-executor-thread-pool-");
-        threadPoolTaskExecutor.setThreadNamePrefix("short-lived-exec-thread-");
-        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        threadPoolTaskExecutor.setAwaitTerminationSeconds(5);
-        threadPoolTaskExecutor.setAllowCoreThreadTimeOut(true);
-        threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        threadPoolTaskExecutor.initialize();
+        shortLivedTaskExecutor.setCorePoolSize(PROCESSORS_COUNT);
+        shortLivedTaskExecutor.setMaxPoolSize(PROCESSORS_COUNT * 2);
+        shortLivedTaskExecutor.setKeepAliveSeconds(10);
+        shortLivedTaskExecutor.setQueueCapacity(100);
+        shortLivedTaskExecutor.setThreadGroupName("short-lived-executor-thread-pool-");
+        shortLivedTaskExecutor.setThreadNamePrefix("short-lived-exec-thread-");
+        shortLivedTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        shortLivedTaskExecutor.setAwaitTerminationSeconds(5);
+        shortLivedTaskExecutor.setAllowCoreThreadTimeOut(true);
+        shortLivedTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        shortLivedTaskExecutor.initialize();
 
-        return threadPoolTaskExecutor;
+        return shortLivedTaskExecutor;
     }
 }
