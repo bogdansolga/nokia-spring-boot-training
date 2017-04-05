304e626f44d8ac16c4214e6e0cf6c9c4fcb2511b
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-05 10:40:57 +0300

    [temp] Temporary commented the @Configuration annotation

--- d02/d02s05/d02s05e01-embedded-database/src/main/java/com/nokia/springboot/training/d02/s05/config/DataSourceConfig.java
+++ d02/d02s05/d02s05e01-embedded-database/src/main/java/com/nokia/springboot/training/d02/s05/config/DataSourceConfig.java
@@ -1,14 +1,12 @@
 package com.nokia.springboot.training.d02.s05.config;
 
 import com.nokia.springboot.training.d02.s05.model.Product;
-import com.nokia.springboot.training.d02.s05.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,7 +15,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.Collections;
 
@@ -28,7 +25,7 @@ import java.util.Collections;
  *     <li>inserts a simple {@link Product} in the auto-wired [in-memory] database</li>
  * </ul>
  */
-@Configuration
+//@Configuration
 @EnableJpaRepositories(
         basePackages = "com.nokia.springboot.training.d02.s05.repository",
         entityManagerFactoryRef = "entityManagerFactory"
@@ -37,11 +34,10 @@ import java.util.Collections;
 public class DataSourceConfig {
 
     private final DataSource dataSource;
-    private final ProductService productService;
 
     @Autowired
-    public DataSourceConfig(final DataSource dataSource, final ProductService productService) {
-        this.dataSource = dataSource; this.productService = productService;
+    public DataSourceConfig(final DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -76,12 +72,5 @@ public class DataSourceConfig {
         emf.setPackagesToScan("com.oce.springboot.training.d02.s05.repository");
 
         return emf;
-    }
-
-    @PostConstruct
-    public void init() {
-        final Product product = new Product();
-        product.setName("A default product");
-        productService.create(product);
     }
 }
