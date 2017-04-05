57376bddbc3e611fdf9167461dbade62dc77730e
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-05 11:16:05 +0300

    [clean] Removed the thread pool config and it's usage

--- d02/d02s05/d02s05e01-embedded-database/src/main/java/com/nokia/springboot/training/d02/s05/repository/ProductRepository.java
+++ d02/d02s05/d02s05e01-embedded-database/src/main/java/com/nokia/springboot/training/d02/s05/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.nokia.springboot.training.d02.s05.model.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,7 +22,6 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
 
     List<Product> findByPrice(final double price);
 
-    @Async("theOtherExecutor")
     Future<Product> findProductById(int id);
 
     @Query(value = "SELECT product FROM Product product WHERE product.name LIKE :name")
