bf396fca42804d3158167eb84b69a187f7ee2131
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-06 18:00:17 +0300

    [dev] Added a sample for the Spring Retry library

---
+++ d04/d04s04/src/main/java/com/nokia/springboot/training/d04/s04/SpringRetryDemo.java
@@ -0,0 +1,17 @@
+package com.nokia.springboot.training.d04.s04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * A small Spring Boot demo for packaging and running the app
+ *
+ * @author bogdan.solga
+ */
+@SpringBootApplication
+public class SpringRetryDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRetryDemo.class, args);
+    }
+}
