15591866a42dd272674c0e7db018177e5501564e
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-06 16:56:10 +0300

    [dev] Added a sample of how to use the @AuthenticationPrincipal annotation

--- d03/d03s02/src/main/java/com/nokia/springboot/training/d03/s02/ProductController.java
+++ d03/d03s02/src/main/java/com/nokia/springboot/training/d03/s02/ProductController.java
@@ -1,21 +1,26 @@
 package com.nokia.springboot.training.d03.s02;
 
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
-    @PreAuthorize("isFullyAuthenticated() AND hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
-    public void addProduct() {
-
-    }
-
-    public void processInternalRequest(@RequestHeader(value = "SECURITY_TOKEN") final String securityToken) {
-        if (securityToken.isEmpty()) {
-            throw new AccessDeniedException("Nope");
-        }
+    @PreAuthorize(
+            "isFullyAuthenticated() " +
+                "AND " +
+            "hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')"
+    )
+    @RequestMapping(
+            path = "/product",
+            method = RequestMethod.GET
+    )
+    public void getProduct(@AuthenticationPrincipal UserDetails userDetails) {
+        final String currentUserName = userDetails.getUsername();
+        System.out.println("The current user is '" + currentUserName + "'");
     }
 }
