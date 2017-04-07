f8ff9bf723bc37299d09c35f75dd8e6eeeae6995
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-07 06:47:47 +0300

    [dev] Added two more profiles

--- d01/d01s05/d01s05e02-bean-profiles/src/main/java/com/nokia/springboot/training/d01/s05/config/Profiles.java
+++ d01/d01s05/d01s05e02-bean-profiles/src/main/java/com/nokia/springboot/training/d01/s05/config/Profiles.java
@@ -3,9 +3,14 @@ package com.nokia.springboot.training.d01.s05.config;
 /**
  * The currently defined Spring {@link org.springframework.context.annotation.Profile}s
  */
+@SuppressWarnings("unused")
 public final class Profiles {
 
     public static final String DEV = "dev";
 
     public static final String PROD = "prod";
+
+    public static final String QA = "qa";
+
+    public static final String NOT = "!";
 }
